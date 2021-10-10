34bed240ad132582f30b23741ad54f081b9a5ec1
Gui-dev <adrianon10@gmail.com>
2021-10-10 19:38:29 -0300

    feat: add screen Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,42 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList } from 'react-native'
 
-import { Background } from '../../components/Background'
-
-// import { Container } from './styles'
+import { ProductItem } from '../../components/ProductItem'
+import { Container, Content } from './style'
 
 export const Home = () => {
+  const products = [
+    {
+      id: '1',
+      createdAt: '2019-09-02T12:58:54.103Z',
+      name: 'Rustic Metal Fish',
+      price: '289.00',
+      image: 'http://lorempixel.com/640/480/food',
+      stock: 65171
+    },
+    {
+      id: '2',
+      createdAt: '2019-09-02T12:58:54.103Z',
+      name: 'Rustic Metal Fish',
+      price: '289.00',
+      image: 'http://lorempixel.com/640/480/food',
+      stock: 65171
+    }
+  ]
+
   return (
-    <Background>
-      <Text>HOME</Text>
-    </Background>
+    <Container>
+      <Content>
+        <FlatList
+          data={ products }
+          keyExtractor={ item => item.id }
+          renderItem={({ item }) => {
+            return (
+              <ProductItem key={ String(item.id) } product={ item }/>
+            )
+          }}
+        />
+      </Content>
+    </Container>
   )
 }
---
+++ src/screens/Home/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native'
+
+import { colors } from './../../styles/global'
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${colors['gray-900']};
+`
+
+export const Content = styled.View`
+  margin: 15px 5%;
+`
