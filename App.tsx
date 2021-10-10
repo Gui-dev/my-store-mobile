85f2376435ce11f2b85bfe604047c6c7b362b49a
Gui-dev <adrianon10@gmail.com>
2021-10-10 19:40:55 -0300

    feat: add component Statusbar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
+import { Header } from './src/components/Header'
 import { Routes } from './src/routes'
 
-export default function App() {
+export default function App () {
   return (
     <NavigationContainer>
+      <StatusBar hidden/>
+      <Header />
       <Routes />
     </NavigationContainer>
   )
