3c93c7c38be2db7c0dc25d15e1233182ce2c5bea
Gui-dev <adrianon10@gmail.com>
2021-10-10 19:33:25 -0300

    feat: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+export const text = {
+  heading: '20px',
+  extraHeading: '35px',
+  normal: '16px'
+}
+
+export const colors = {
+  'gray-900': '#121212',
+  'gray-800': '#1A1A1A',
+  'gray-600': '#696969',
+  'gray-100': '#C9C9C9',
+  'green-500': '#3EB595',
+  'yellow-500': '#FFF447',
+  white: '#FFF'
+}
