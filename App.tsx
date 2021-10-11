18eedfd41194e5d34492fa2a0a55e68b882d1379
Gui-dev <adrianon10@gmail.com>
2021-10-11 14:00:38 -0300

    feat: add context provider CartProvider

--- App.tsx
+++ App.tsx
@@ -2,15 +2,19 @@ import React from 'react'
 import { StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
+import { CartProvider } from './src/contexts/CartContext'
+
 import { Header } from './src/components/Header'
 import { Routes } from './src/routes'
 
 export default function App () {
   return (
-    <NavigationContainer>
-      <StatusBar hidden/>
-      <Header />
-      <Routes />
-    </NavigationContainer>
+    <CartProvider>
+      <NavigationContainer>
+        <StatusBar hidden/>
+        <Header />
+        <Routes />
+      </NavigationContainer>
+    </CartProvider>
   )
 }
