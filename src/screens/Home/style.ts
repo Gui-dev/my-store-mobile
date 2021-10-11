108253ee6e829bc44e894e9a0bbbc743939b777e
Gui-dev <adrianon10@gmail.com>
2021-10-11 02:05:44 -0300

    style: add flex 1 and background

--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -4,9 +4,10 @@ import { colors } from './../../styles/global'
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${colors['gray-900']};
+  background-color: ${colors['gray-800']};
 `
 
 export const Content = styled.View`
+  flex: 1;
   margin: 15px 10%;
 `
